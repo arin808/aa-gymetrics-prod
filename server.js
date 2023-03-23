71f98bee6bce6f549745376d510b44fe1df71827
Arin <71049312+arin808@users.noreply.github.com>
2023-03-22 19:59:32 -0700

    Troubleshooting

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 //Dependencies and requirements to setup DB
-const path = require("path");
+const { mongoConnect } = require("./config/dbconfig");
+const port = (process.env.PORT || 3000);
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
 const cors = require("cors");
 app.use(cors());
-const mongoose = require("mongoose");
-const port = (process.env.PORT || 3000);
-const bodyParser = require("body-parser");
-require("dotenv/config");
-
-const mongoURI = process.env.MONGO_URI;
 
 //Route requirements
 const StudentRoutes = require("./routes/api/StudentsRoutes");
@@ -18,28 +15,31 @@ const LoginRoutes = require("./routes/api/LoginsRoutes");
 const ActiveRoutes = require("./routes/api/ActiveRoutes");
 
 
-//Utilize parser for requests
-app.use(express.static(path.join(__dirname + "/dist")));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+//Initiate app connection to MongoDB
+async function startServer() {
+  await mongoConnect();
+  
+  
+  //Utilize parser for requests
+  app.use(express.static(path.join(__dirname + "/dist")));
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  
+  //API route connections
+  app.use("/students", StudentRoutes);
+  app.use("/employees", EmployeeRoutes);
+  app.use("/logins", LoginRoutes);
+  app.use("/actives", ActiveRoutes);
 
-//API route connections
-app.use("/students", StudentRoutes);
-app.use("/employees", EmployeeRoutes);
-app.use("/logins", LoginRoutes);
-app.use("/actives", ActiveRoutes);
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/dist/index.html"));
+  });
 
-mongoose
-.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+  //Listen for active app on Port 3000
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
-});
-
-//Listen for active app on Port 3000
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
-
+//Start the app
+startServer();
