08a3037568a09a55a7b3bf142080481cbcc4023d
Arin <71049312+arin808@users.noreply.github.com>
2023-03-22 21:06:30 -0700

    server update

--- server.js
+++ server.js
@@ -16,25 +16,30 @@ const ActiveRoutes = require("./routes/api/ActiveRoutes");
 
 
 //Initiate app connection to MongoDB
+async function startServer() {
+  await mongoConnect();
+  
+  
+  //Utilize parser for requests
+  app.use(express.static(path.join(__dirname + "/dist")));
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  
+  //API route connections
+  app.use("/students", StudentRoutes);
+  app.use("/employees", EmployeeRoutes);
+  app.use("/logins", LoginRoutes);
+  app.use("/actives", ActiveRoutes);
 
-//Utilize parser for requests
-app.use(express.static(path.join(__dirname + "/dist")));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/dist/index.html"));
+  });
 
-//API route connections
-app.use("/students", StudentRoutes);
-app.use("/employees", EmployeeRoutes);
-app.use("/logins", LoginRoutes);
-app.use("/actives", ActiveRoutes);
+  //Listen for active app on Port 3000
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}
 
-await mongoConnect();
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
-});
-
-//Listen for active app on Port 3000
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+//Start the app
+startServer();
